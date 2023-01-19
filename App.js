5be4a8412788ba8686cca1c06dc8278baa9c3cfc
yash <yash.dadhich@squarebits.com>
2023-01-19 13:25:24 +0530

    Functionality added

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native"
 import Task from "./components/task"
 
 export default function App() {
+  const [task, setTask] = useState()
+
+  const [taskItems, setTaskItems] = useState([])
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null)
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems]
+    itemsCopy.splice(index, 1)
+    setTaskItems(itemsCopy)
+  }
+
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
@@ -10,11 +33,34 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's Tasks</Text>
 
         <View style={styles.items}>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            )
+          })}
+
           {/* This is where tasks will go */}
-          <Task text={"Task 1"} />
-          <Task text={"Task 2"} />
         </View>
       </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(task) => setTask(task)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   )
 }
@@ -40,4 +86,33 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
   },
+  writeTaskWrapper: {
+    position: "absolute",
+    width: "100%",
+    bottom: 36,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 40,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: "78%",
+  },
+  addWrapper: {
+    width: 50,
+    height: 50,
+    borderRadius: 60,
+    borderWidth: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 })
